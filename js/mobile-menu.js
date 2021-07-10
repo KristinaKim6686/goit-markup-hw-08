34c0c6d911ca1b2ed3bf16e87e78cc70600f9e03
KristinaKim6686 <KristinaKim6686@outlook.com>
2021-07-11 02:09:24 +0300

    ficsing all the time

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openMenuBtn: document.querySelector('.menu-open'),
+    closeMenuBtn: document.querySelector('.menu-close'),
+    menu: document.querySelector('.mobile-menu'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
   }
 })();
